80706541cebced5fac64940eec798fadacc5bf79
ANGEL <ANGEL>
2017-01-17 23:55:21 -0500

    PRIMERA CARGA

---
+++ PetCenter/PetCenter/Pet/Pet.Web/Controllers/SolicitudMantenimientoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pet.Web.Controllers
+{
+    public class SolicitudMantenimientoController : Controller
+    {
+        //
+        // GET: /SolicitudMantenimiento/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
